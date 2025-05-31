0a8b10833d8ab262d0142fef36b5a4317da47415
isha-iitm <isha.iitm@gmail.com>
2025-05-31 11:06:55 +0530

    Add files via upload

---
+++ train.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import metrics
+import os
+import pickle
+
+
+data = pd.read_csv('iris.csv')
+
+train, test = train_test_split(data, test_size = 0.4, stratify = data['species'], random_state = 42)
+X_train = train[['sepal_length','sepal_width','petal_length','petal_width']]
+y_train = train.species
+X_test = test[['sepal_length','sepal_width','petal_length','petal_width']]
+y_test = test.species
+
+print('training started ........')
+mod_dt = DecisionTreeClassifier(max_depth = 3, random_state = 1)
+mod_dt.fit(X_train,y_train)
+prediction=mod_dt.predict(X_test)
+print('training ended .......')
+print('The accuracy of the Decision Tree is',"{:.3f}".format(metrics.accuracy_score(prediction,y_test)))
+
+
+os.makedirs('models', exist_ok=True)
+with open('models/week2_model.pkl', 'wb') as f:
+    pickle.dump(mod_dt, f)
+print('Model saved')
